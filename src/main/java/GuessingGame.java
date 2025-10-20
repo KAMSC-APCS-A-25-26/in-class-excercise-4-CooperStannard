c7a7feac2501ec864b28c2390398377430f64db0
CooperStannard <03cooper.stannard@kamsc.org>
2025-10-20 13:45:48 -0400

    g

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -11,27 +11,23 @@ public class GuessingGame {
         Scanner sc = new Scanner(System.in);
         boolean run = true;
         int num= rand.nextInt(100)+1;
-        while (run)
-        {
+        do {
             System.out.print("Enter your guess: ");
             int gNum = sc.nextInt();
-            if (gNum > num)
-            {
+            if (gNum > num) {
                 System.out.println("Too high! Try again.");
-            } else if (gNum < num)
-            {
+            } else if (gNum < num) {
                 System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("You got it!");
+                run = false;
             }
-            else
-            {
-              System.out.println("You got it!");
-              run = false;
-            }
-
+        }
+            while (run);
             // TODO: Generate a random secret number between 1 and 100 (inclusive)
             // TODO: Use a do...while loop to read guesses until correct
             // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
             // TODO: Use Scanner for input
-        }
+
     }
 }
--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class ShoppingCart {
     public static void main(String[] args) {
         // TODO: Ask the user how many items they are purchasing
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many items are you purchasing? ");
+        int times = sc.nextInt();
+        double total = 0;
         // TODO: Use a for loop to read each item's price (double)
+        for (int i= 0; i< times; i++){
+            System.out.print("Price of item " + (i+1) + "? ");
+            total += sc.nextDouble();
+        }
+        System.out.println("Your cart has " +times + " items with a total cost of $" + total);
         // TODO: Accumulate the total cost
         // TODO: Print the final summary line with item count and total
         // TODO: Use Scanner for input
